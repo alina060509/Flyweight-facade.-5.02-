295ed4a2521da4767a0d109108de418065ea39e1
alinka0605@icloud.com <alinka0605@icloud.com>
2025-02-10 11:17:34 +0300

    Добавьте файлы проекта.

---
+++ Flyweight и facade.ДЗ(5.02)/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Facade;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kitchen = new KitchenSystem();
+            var delivery = new DeliverySystem();
+            var payment = new PaymentSystem();
+
+            var facade = new PizzaOrderFacade(kitchen, delivery, payment);
+            facade.WatchMovi();
+            Console.WriteLine("приятного аппетита....");
+
+        }
+    }
+
+    public class KitchenSystem
+    {
+        public void PreparesPizza()
+        {
+            Console.WriteLine("Готовит пиццу!");
+        }
+        public void CollectsOrder()
+        {
+            Console.WriteLine("Собирает заказ!");
+        }
+    }
+}
+public class DeliverySystem
+{
+    public void Delivers()
+    {
+        Console.WriteLine("Доставляет заказ клиенту!");
+    }
+    public void WaitingPayment()
+    {
+        Console.WriteLine("Ждет оплаты!");
+    }
+}
+public class PaymentSystem
+{
+    public void AcceptsPayment()
+    {
+        Console.WriteLine("Принимает оплату!");
+    }
+    public void PassedPayment()
+    {
+        Console.WriteLine("Оплата прошла!");
+    }
+}
+public class PizzaOrderFacade
+{
+    private readonly KitchenSystem _kitchenSystem;
+    private readonly DeliverySystem _deliverySystem;
+    private readonly PaymentSystem _paymentSystem;
+
+    public PizzaOrderFacade(KitchenSystem kitchen, DeliverySystem delivery, PaymentSystem payment)
+    {
+        _kitchenSystem = kitchen;
+        _deliverySystem = delivery;
+        _paymentSystem = payment;
+    }
+
+    public void WatchMovi()
+    {
+        _kitchenSystem.PreparesPizza();
+        _kitchenSystem.CollectsOrder();
+        _deliverySystem.Delivers();
+        _deliverySystem.WaitingPayment();
+        _paymentSystem.AcceptsPayment();
+        _paymentSystem.PassedPayment();
+    }
+}
---
+++ flyweight/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Flower> flowers = new List<Flower>();
+            for (int i = 0; i < 3; i++)
+            {
+                var FlowerType = FlowerFactory.GetTreeType("Тюльпан", "желтый", "мягкая кора");
+                var FlowerType2 = FlowerFactory.GetTreeType("Ромашка", "белая", "мягко-шершавая кора");
+                flowers.Add(new Flower(i * 15, i * 21, FlowerType));
+                flowers.Add(new Flower(i * 16, i * 20, FlowerType2));
+            }
+            foreach (var tree in flowers)
+            {
+                tree.Display();
+            }
+        }
+    }
+    public class FlowerType
+    {
+        public string Name { get; }
+        public string Color { get; }
+        public string Texture { get; }
+
+        public FlowerType(string name, string color, string texture)
+        {
+            Name = name;
+            Color = color;
+            Texture = texture;
+        }
+        public void Display(int x, int y)
+        {
+            Console.WriteLine($" Цветок с типом {Name} с цветом {Color} и текстурой {Texture} находитя на координатах({x},{y})");
+        }
+    }
+    public class Flower
+    {
+        private readonly int _x;
+        private readonly int _y;
+        private readonly FlowerType _type;
+
+        public Flower(int x, int y, FlowerType type)
+        {
+            _x = x;
+            _y = y;
+            _type = type;
+        }
+        public void Display()
+        {
+            _type.Display(_x, _y);
+
+        }
+    }
+    public class FlowerFactory
+    {
+        private static readonly Dictionary<string, FlowerType> _flowerTypes = new Dictionary<string, FlowerType>();
+        public static FlowerType GetTreeType(string name, string color, string texture)
+        {
+            string key = $"{name}_{color}_{texture}";
+            if (!_flowerTypes.ContainsKey(key))
+            {
+                _flowerTypes[key] = new FlowerType(name, color, texture);
+                Console.WriteLine($"Создан новый ключ: {key}");
+            }
+            return _flowerTypes[key];
+        }
+    }
+
+}
+
